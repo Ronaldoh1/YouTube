3b5baa1ca3d578bce2d68ef6051b90835cf516be
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-09-11 02:14:43 -0500

    Initial Commit

--- YouTube/AppDelegate.swift
+++ YouTube/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // get the window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // make the window visible
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
