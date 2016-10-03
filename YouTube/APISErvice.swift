fa00d321bbbba6201f29d9294bb8d871b7e1f1bf
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-10-02 21:24:17 -0500

    refactor code so that we separate the service layer from the view controller

---
+++ YouTube/APISErvice.swift
@@ -0,0 +1,62 @@
+//
+//  APISErvice.swift
+//  YouTube
+//
+//  Created by Ronald Hernandez on 10/2/16.
+//  Copyright © 2016 Ronaldoh1. All rights reserved.
+//
+import UIKit
+
+class APIService: NSObject {
+    
+    static let sharedInstance = APIService()
+    
+    func fetchVideos(completion: @escaping ([Video]) -> ()) {
+        let url = URL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
+        let request = URLRequest(url: url!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {data,response,error in
+            if error != nil {
+                print(error)
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                print(json)
+                
+                var videos = [Video]()
+                for dictionary in json as! [[String : AnyObject]]{
+                    
+                    let video = Video()
+                    
+                    video.title = dictionary["title"] as? String
+                    //                    let numberOfViewRaw = dictionary["number_of_views"] as? String
+                    //                    video.numberOfViews = NSNumber(numberOfViewRaw
+                    video.thumbNailImageName = dictionary["thumbnail_image_name"] as? String
+                    
+                    let channelDictionary = dictionary["channel"] as? [String : AnyObject]
+                    
+                    let channel = Channel()
+                    
+                    channel.name = channelDictionary?["name"] as? String
+                    channel.profileImageName = channelDictionary?["profile_image_name"] as? String
+                    
+                    video.channel = channel
+                    
+                    videos.append(video)
+                    
+                }
+                
+                //Bounce back to the main thread and upadate the UI
+                
+//                self.collectionView?.reloadData()
+                completion(videos)
+                
+                
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+        }
+        task.resume()
+    }
+}
--- YouTube/HomeViewController.swift
+++ YouTube/HomeViewController.swift
@@ -73,51 +73,10 @@ class HomeViewController: UICollectionViewController {
     }
     
     private func fetchVideos() {
-        let url = URL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
-        let request = URLRequest(url: url!)
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {data,response,error in
-            if error != nil {
-                print(error)
-            }
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-                print(json)
-                
-                self.videos = [Video]()
-                for dictionary in json as! [[String : AnyObject]]{
-                    
-                    let video = Video()
-                    
-                    video.title = dictionary["title"] as? String
-                    //                    let numberOfViewRaw = dictionary["number_of_views"] as? String
-                    //                    video.numberOfViews = NSNumber(numberOfViewRaw
-                    video.thumbNailImageName = dictionary["thumbnail_image_name"] as? String
-                    
-                    let channelDictionary = dictionary["channel"] as? [String : AnyObject]
-                    
-                    let channel = Channel()
-                    
-                    channel.name = channelDictionary?["name"] as? String
-                    channel.profileImageName = channelDictionary?["profile_image_name"] as? String
-                    
-                    video.channel = channel
-                    
-                    self.videos?.append(video)
-                    
-                }
-                
-                //Bounce back to the main thread and upadate the UI
-                
-                self.collectionView?.reloadData()
-                
-                
-                
-            } catch let jsonError {
-                print(jsonError)
-            }
-            
+        APIService.sharedInstance.fetchVideos { (videos: [Video]) in
+            self.videos = videos
+            self.collectionView?.reloadData()
         }
-        task.resume()
     }
     
     // Add black view to the entire window.
