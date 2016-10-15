0b86f38cc34ac13da4f17ad39e94b471a4f99b4e
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-10-15 18:31:56 -0500

    added player controller

--- YouTube/VideoPlayerView.swift
+++ YouTube/VideoPlayerView.swift
@@ -12,24 +12,92 @@ import AVFoundation
 
 class VideoPlayerView: UIView {
     
+    let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
+        indicator.translatesAutoresizingMaskIntoConstraints = false // since we're going to add it programmatically
+        indicator.startAnimating()
+        return indicator
+    }()
+    
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        return view
+    }()
+    
+    lazy var pausePayButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "pause")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        return button
+    }()
+    var player: AVPlayer?
+    var isPlaying = false
+    
+    func handlePause() {
+        if isPlaying {
+            player?.pause()
+            pausePayButton.setImage(UIImage(named: "play"),for: UIControlState.normal)
+        } else {
+            player?.play()
+        }
+      
+        isPlaying = !isPlaying
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupPlayerView()
+        
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicator)
+        activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+
+        activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePayButton)
+        
+        pausePayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pausePayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         backgroundColor = UIColor.black
-        let urlString = "https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/HT1425/sample_iPod.m4v.zip"
-        if let url = URL(string: urlString) {
-        let player = AVPlayer(url: url)
-            
-            let playerLayer = AVPlayerLayer(player: player)
-            self.layer.addSublayer(playerLayer)
-            
-            playerLayer.frame = self.frame
-            
-            player.play()
-        }
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupPlayerView() {
+        let urlString = "https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/HT1425/sample_iPod.m4v"
+        if let url = URL(string: urlString) {
+            
+            player = AVPlayer(url: url)
+            
+            let playerLayer = AVPlayerLayer(player: player)
+            self.layer.addSublayer(playerLayer)
+            
+            playerLayer.frame = self.frame
+            
+            player?.play()
+            
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicator.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePayButton.isHidden = false
+            isPlaying = true
+            
+        }
+    }
 }
